29ce43e45f9cab219f50b02935183837a9a5deba
Clement <clement.fraissinet@orange.fr>
2023-04-27 16:37:08 +0200

    debut de la partie home de la page home avec titre + input + compteur dynamique + button(pas en marche)

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 function Home() {
+  const [tweet, setTweet] = useState("");
+  let nbrChar = tweet.length;
   return (
     <div>
       <div className={styles.container}>
@@ -8,7 +11,20 @@ function Home() {
           <h3>User</h3>
         </div>
         <div className={styles.home}>
-          <h3>Home</h3>
+          <div className={styles.headerHome}>
+            <h3>Home</h3>
+            <input
+              className={styles.input}
+              type="text"
+              onChange={(e) => setTweet(e.target.value)}
+              value={tweet}
+              placeholder="What's up?"
+            />
+            <div className={styles.botHeaderHome}>
+              <p>{nbrChar}/280</p>
+              <button className={styles.buttonTweet}>Tweet</button>
+            </div>
+          </div>
         </div>
         <div className={styles.hashtag}>
           <h3>Trend</h3>
