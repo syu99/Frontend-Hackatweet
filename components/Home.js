765d08ec4bc1f5ca590c3d1d978635eb8dbb28be
Clement <clement.fraissinet@orange.fr>
2023-04-27 14:55:46 +0200

    structure page home

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,19 @@
-import styles from '../styles/Home.module.css';
-
+import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
     <div>
-      <main className={styles.main}>
-      <h1>Home component</h1>
-      </main>
+      <div className={styles.container}>
+        <div className={styles.user}>
+          <h3>User</h3>
+        </div>
+        <div className={styles.home}>
+          <h3>Home</h3>
+        </div>
+        <div className={styles.hashtag}>
+          <h3>Trend</h3>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@ import "../styles/globals.css";
 import Head from "next/head";
 import Modal from "../components/Modal";
 import Login from "../components/Login";
+import Home from "../components/Home";
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <Login />
+      <Home />
     </>
   );
 }
