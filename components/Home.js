6c3eb808608c2462a47de04651b17af77a054670
Clement <clement.fraissinet@orange.fr>
2023-04-28 11:36:50 +0200

    afficher tweet + créatio de tweet sans #

--- components/Home.js
+++ components/Home.js
@@ -1,20 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
-const handleLogout = () => {
-  dispatch(logout());
-};
-
 function Home() {
   const [tweet, setTweet] = useState("");
+  const [dataTweet, setDataTweet] = useState([]);
+  const [majTweet, setMajTweet] = useState(false);
+
+  useEffect(() => {
+    console.log("new tweet");
+    fetch("http://localhost:3000/tweets")
+      .then((response) => response.json())
+      .then((data) => {
+        setDataTweet([...data.tweets]);
+        //console.log(data);
+      });
+  }, [majTweet]);
+
+  console.log(dataTweet);
+
   let nbrChar = tweet.length;
 
   let firstName = "firstName";
   let userName = "userName";
+  //action de donnection
+  const handleLogout = () => {
+    dispatch(logout());
+  };
 
+  //action créer un tweet
+
+  //affichage des tweets dans le front
+  const listTweets = dataTweet.map((data, i) => {
+    return (
+      <div className={styles.tweet}>
+        <div className={styles.titreUser}>
+          <img
+            src="profile-egg.png"
+            alt="Egg logo"
+            className={styles.egglogo}
+          />
+          <p>
+            {data.firstname} @{data.username}
+          </p>
+        </div>
+        <div className={styles.contenuTweet}>{data.tweet}</div>
+      </div>
+    );
+  });
+
+  function createTweet() {
+    fetch("http://localhost:3000/tweets", {
+      method: "POST",
+
+      headers: { "Content-Type": "application/json" },
+
+      body: JSON.stringify({
+        username: "username",
+        firstname: "firstname",
+        tweet: tweet,
+        hashtag: "",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMajTweet(!majTweet);
+      });
+  }
   return (
-    <div>
+    <div className={styles.main}>
       <div className={styles.container}>
         <div className={styles.user}>
           <Link href="/home">
@@ -58,9 +113,15 @@ function Home() {
             />
             <div className={styles.botHeaderHome}>
               <p>{nbrChar}/280</p>
-              <button className={styles.buttonTweet}>Tweet</button>
+              <button
+                className={styles.buttonTweet}
+                onClick={() => createTweet()}
+              >
+                Tweet
+              </button>
             </div>
           </div>
+          <div className={styles.bodyHome}>{listTweets}</div>
         </div>
         <div className={styles.hashtag}>
           <h3>Trend</h3>
