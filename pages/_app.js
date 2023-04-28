0d7311c55ecd4d6dec93f1d96e741cd033981bb1
Clement <clement.fraissinet@orange.fr>
2023-04-28 13:52:32 +0200

    refont structure page

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,35 @@ import Login from "../components/Login";
 import Home from "../components/Home";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import user from "../reducers/user";
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const reducers = combineReducers({ user });
+
+const persistConfig = { key: "hackatweet", storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
-    <>
-      <Home />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>Morning News</title>
+        </Head>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Home from "../components/Home";
 import Login from "../components/Login";
 
 function Index() {
-  return <Home />;
+  return <Login />;
 }
 
 export default Index;
